e794788692654295755dce5bf65af83fad4963ab
Taisiya Tomashevska <taya22011985@gmail.com>
2023-02-11 16:12:34 +0200

    push

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-const form = document.querySelector('form');
-const list = document.querySelector('.list-js')
+const form = document.querySelector("form");
+const renderAdd = document.querySelector(".js-list");
 
-form.addEventListener('submit', getWeather);
+form.addEventListener("submit", getWeather);
 
-function getFetch(city, days) { 
-    return fetch(`http://api.weatherapi.com/v1/forecast.json?key=48cb2a8d61c54182a7c120347230702&q=${city}&days=${days}`)
-        .then(res => {
-            if (!res.ok) {
-                throw new Error(res.statusText)
-            }
-            return res.json();
-         } )
-    
+function getWeather(e) {
+	e.preventDefault();
+	const input = e.currentTarget.elements.city.value;
+	const select = e.currentTarget.elements.days.value;
+
+	getFetch(input, select).then((value) => {
+		console.log("getFetch --> value", value);
+		render(value.forecast.forecastday);
+	});
 }
 
-
-function getWeather(e) { 
-    e.preventDefault()
-    const input = form.elements.city.value;
-    const select = form.elements.days.value;
-
-    getFetch(input, select).then((res) => {
-        renderMarkup(res.forecast.forecastday)
-}) 
+function getFetch(city, days) {
+	return fetch(
+		`http://api.weatherapi.com/v1/forecast.json?key=06e945ae9a224778ae3134342232801&q=${city}&days=${days}`
+	).then((res) => {
+		if (!res.ok) {
+			throw new Error(res.statusText);
+		}
+		return res.json();
+	});
 }
 
-function renderMarkup(forecast) {
-    const markup = forecast.map(({ date, day: { avgtemp_c, condition: { icon } } }) => {
-        return `<ul>
-    <li>${date}</li>
-    <li>${avgtemp_c}</li>
-    <img src="${icon}">
-</ul>`
-    }).join('');
-    list.innerHTML = markup;
- }
+function render(array) {
+	const markup = array
+		.map(
+			({
+				date,
+				day: {
+					avgtemp_c,
+					condition: { icon },
+				},
+			}) => {
+				return `
+          <li>
+            <p>${date}</p>
+            <p>${avgtemp_c}</p>
+            <img src="${icon}"></img>
+          </li>`;
+			}
+		)
+		.join("");
+	renderAdd.innerHTML = markup;
+}
